76842831be58d8632b201a8a5ae01fe760bb6085
Graehm <gpatrellfazio@gmail.com>
2024-03-25 16:48:22 -0500

    deleted extra fetch data that was making api calls every key stroke in the input field

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ const Weather = () => {
       if(!city) return
       const apiKey = import.meta.env.VITE_REACT_APP_APIKEY
       const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-        //  `http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid=${apiKey}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${'ca'},${'us'}&units=imperial&appid=${apiKey}`
+        // `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=&{apiKey}`
       );
       setWeatherData(response.data);
       console.log(response.data);
@@ -21,9 +21,10 @@ const Weather = () => {
     }
   };
 
-  useEffect(() => {
-    fetchData();
-  }, [city]);
+// dont need because it fetches a city every key stroke 
+  // useEffect(() => {
+  //   fetchData();
+  // }, [city]);
 
   const handleInputChange = (e) => {
     setCity(e.target.value);
@@ -43,14 +44,14 @@ const Weather = () => {
           value={city}
           onChange={handleInputChange}
         />
-        <button type="submit">Get Weather</button>
+        <button type="submit">Road Cast Forecast</button>
       </form>
       {weatherData ? (
         <>
           <h2>{weatherData.name}</h2>
           <p>Temperature: {weatherData.main.temp}°F</p>
           <p>Description: {weatherData.weather[0].description}</p>
-          <p>Feels like : {weatherData.main.feels_like}°F</p>
+          {/* <p>Feels like : {weatherData.main.feels_like}°F</p> */}
           <p>Humidity : {weatherData.main.humidity}%</p>
           <p>Wind Speed : {weatherData.wind.speed}mph</p>
         </>
